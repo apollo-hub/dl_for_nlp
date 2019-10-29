14ef0807c5dcf2cf8bce44b36ca8beecd5ab122d
Koji Seto <kojiseto@apolloaisystems.com>
2019-10-29 16:27:44 -0700

    sample_models.py updated

--- sample_models.py
+++ sample_models.py
@@ -38,3 +38,30 @@ def rnn_model(input_dim, units, activation, output_dim=29):
     model.output_length = lambda x: x
     print(model.summary())
     return model
+
+
+def cnn_rnn_model(input_dim, filters, kernel_size, conv_stride,
+    conv_border_mode, units, output_dim=29):
+    """ Build a recurrent + convolutional network for speech 
+    """
+    # Main acoustic input
+    input_data = Input(name='the_input', shape=(None, input_dim))
+    # TODO: Add convolutional layer
+    conv_1d = input_data # TODO: Modify this line
+    # Add batch normalization
+    bn_cnn = BatchNormalization(name='bn_conv_1d')(conv_1d)
+    # Add a recurrent layer
+    simp_rnn = SimpleRNN(units, activation='relu',
+        return_sequences=True, implementation=2, name='rnn')(bn_cnn)
+    # Add batch normalization
+    bn_rnn = BatchNormalization(name='bn_rnn')(simp_rnn)
+    # Add a TimeDistributed(Dense(output_dim)) layer
+    time_dense = TimeDistributed(Dense(output_dim))(bn_rnn)
+    # Add softmax activation layer
+    y_pred = Activation('softmax', name='softmax')(time_dense)
+    # Specify the model
+    model = Model(inputs=input_data, outputs=y_pred)
+    model.output_length = lambda x: cnn_output_length(
+        x, kernel_size, conv_border_mode, conv_stride)
+    print(model.summary())
+    return model
